529615c8e9c020349bd3cfb3b96018abaae7247b
Ziad Sawalha <ziad.sawalha@rackspace.com>
2013-01-22 10:20:06 -0600

    Collection of updates over time
    Sorry, not broken up into separate commits

--- tools/tracer.py
+++ tools/tracer.py
@@ -36,6 +36,7 @@ red.
 
 """
 
+import linecache
 import os
 import sys
 
@@ -46,64 +47,107 @@ if '--trace-calls' in sys.argv or '-t' in sys.argv:
         if sys.argv[i] in ['-t', '--trace-calls']:
             sys.argv.pop(i)
 
+    STACK_DEPTH = 0
+
+    # Calculate root project path
+    POSSIBLE_TOPDIR = os.path.normpath(os.path.join(
+                                       os.path.abspath(sys.argv[0]),
+                                       os.pardir,
+                                       os.pardir))
+
+    class ConsoleColors():
+        HEADER = '\033[95m'
+        OKBLUE = '\033[94m'
+        OKGREEN = '\033[92m'
+        WARNING = '\033[93m'
+        FAIL = '\033[91m'
+        ENDC = '\033[0m'
+
     def localtrace(frame, event, arg):
-        global stack_depth
         if event == "return":
-            stack_depth = stack_depth - 1
+            global STACK_DEPTH  # pylint: disable=W0603
+            STACK_DEPTH = STACK_DEPTH - 1
         elif event == "exception":
-            co = frame.f_code
-            func_name = co.co_name
-            line_no = frame.f_lineno
-            filename = co.co_filename
-            exc_type, exc_value, exc_traceback = arg
-            print '\033[91m%sERROR: %s %s on line %s of %s\033[0m' % \
-                ('  ' * stack_depth, exc_type.__name__, exc_value, line_no,
-                 func_name)
+            output_exception(frame, arg)
         return None
 
-    def selectivetrace(frame, event, arg):
-        global stack_depth
+    def selectivetrace(frame, event, arg):  # pylint: disable=R0911
+        global STACK_DEPTH  # pylint: disable=W0603
         if event == "exception":
+            output_exception(frame, arg)
+        if event == 'call':
             co = frame.f_code
             func_name = co.co_name
-            line_no = frame.f_lineno
-            filename = co.co_filename
-            exc_type, exc_value, exc_traceback = arg
-            print '\033[91m%sERROR: %s %s on line %s of %s\033[0m' % \
-                ('  ' * stack_depth, exc_type.__name__, exc_value, line_no,
-                 func_name)
-        if event != 'call':
-            return
-        co = frame.f_code
-        func_name = co.co_name
-        if func_name == 'write':
-            # Ignore write() calls from print statements
-            return
-        func_filename = co.co_filename
-        if func_filename == "<string>":
-            return
-        if func_filename.startswith("/System"):
-            return
-        if func_filename.startswith("/Library"):
-            return
-        if 'macosx' in func_filename:
-            return
-        func_line_no = frame.f_lineno
-        # If ../..//project/__init__.py exists, add ../ to Python search path, so that
-        # it will override what happens to be installed in /usr/(local/)lib/python...
-        possible_topdir = os.path.normpath(os.path.join(os.path.abspath(sys.argv[0]),
-                                           os.pardir,
-                                           os.pardir))
-        func_filename = func_filename.replace(possible_topdir, '')
-        caller = frame.f_back
-        caller_line_no = caller.f_lineno
-        caller_filename = caller.f_code.co_filename.replace(possible_topdir,
-                                                            '')
-        print '%s%s::%s:%s      (from %s:%s)' % \
-            ('  ' * stack_depth, func_filename, func_name, func_line_no,
-             caller_filename, caller_line_no)
-        stack_depth = stack_depth + 1
-        return localtrace
+            if func_name == 'write':
+                # Ignore write() calls from print statements
+                return
+            func_filename = co.co_filename
+            if func_filename == "<string>":
+                return
+            if func_filename.startswith(("/System", "/Library",
+                                         "/usr/lib/py")):
+                return
+            if 'python' in func_filename:
+                return
+            if 'macosx' in func_filename:
+                return
+            output_call(frame, arg)
+            global STACK_DEPTH  # pylint: disable=W0603
+            STACK_DEPTH = STACK_DEPTH + 1
+            return localtrace
+        return
 
+    def output_exception(frame, arg):
+        exc_type, exc_value, exc_traceback = arg  # pylint: disable=W0612
+        if exc_type is StopIteration:
+            return
+        global STACK_DEPTH  # pylint: disable=W0603
+        global POSSIBLE_TOPDIR  # pylint: disable=W0603
+        co = frame.f_code
+        local_vars = frame.f_locals
+        func_name = co.co_name
+        line_no = frame.f_lineno
+        func_filename = co.co_filename
+        func_filename = func_filename.replace(POSSIBLE_TOPDIR, '')
+        sys.stdout.write('%s%sERROR: %s %s in %s of %s:%s%s\n'
+                              % (ConsoleColors.FAIL, '  ' * STACK_DEPTH,
+                                 exc_type.__name__, exc_value, func_name,
+                                 func_filename, line_no, ConsoleColors.ENDC))
+        filename = co.co_filename
+        if filename == "<stdin>":
+            filename = "%s.py" % __file__
+        if (filename.endswith(".pyc") or
+            filename.endswith(".pyo")):
+            filename = filename[:-1]
+        line = linecache.getline(filename, line_no)
+        name = frame.f_globals.get("__name__")
+        sys.stdout.write('%s%s    %s:%s: %s%s\n' %
+                         (ConsoleColors.HEADER, '  ' * STACK_DEPTH, name,
+                          line_no, line.rstrip(), ConsoleColors.ENDC))
+
+        sys.stdout.write('%s    locals: %s\n'
+                              % ('  ' * STACK_DEPTH,
+                                 local_vars))
+
+    def output_call(frame, arg):
+        caller = frame.f_back
+
+        if caller:
+            global STACK_DEPTH  # pylint: disable=W0603
+            global POSSIBLE_TOPDIR  # pylint: disable=W0603
+            co = frame.f_code
+            func_name = co.co_name
+            func_line_no = frame.f_lineno
+            func_filename = co.co_filename
+            func_filename = func_filename.replace(POSSIBLE_TOPDIR, '')
+            caller_line_no = caller.f_lineno
+            caller_filename = caller.f_code.co_filename.replace(
+                                                        POSSIBLE_TOPDIR, '')
+            if caller_filename == func_filename:
+                caller_filename = 'line'
+            sys.stdout.write('%s%s::%s:%s      (from %s:%s)\n' %
+                ('  ' * STACK_DEPTH, func_filename, func_name, func_line_no,
+                 caller_filename, caller_line_no))
+
+    sys.stdout.write('Starting call tracer\n')
     sys.settrace(selectivetrace)
-    print 'Starting call tracer'
